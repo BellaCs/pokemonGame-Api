ee58d02555c375c91dee405f390f0a098088a40d
Blakasura <dprieto8@boscdelacoma.cat>
2021-03-23 17:49:27 +0000

    Arreglat error != null

--- Server/api/getFormatter/movementFormatter.socket.js
+++ Server/api/getFormatter/movementFormatter.socket.js
@@ -3,8 +3,8 @@ const movementDB = require("../models/movement.database");
 function getMovement(movement_id){
     var movement;
         movementDB.findById(movement_id,(error, result) =>{
-            if(error!= null){
-                movement = JSON.stringify(result);
+            if(error == null){
+                movement = result;
                 console.log("Moviment: " + result);
             }else{
                 console.log(error);
